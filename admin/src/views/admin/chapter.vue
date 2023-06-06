ec37e8a7c8de1d3a6eadf889a54fe9b0fe1b9202
muxin <wgy952046097@gmail.com>
2023-06-06 10:38:58 +0800

    feat: 1.解决gateway跨域 2.使用lb://+注册中心名称作路由转发 loadbalance

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h4 class="lighter">
-      <i class="ace-icon fa fa-hand-o-right icon-animated-hand-pointer blue"></i>
-      <router-link to="/business/course" class="pink"> {{course.name}} </router-link>
+      <i
+        class="ace-icon fa fa-hand-o-right icon-animated-hand-pointer blue"
+      ></i>
+      <router-link to="/business/course" class="pink">
+        {{ course.name }}
+      </router-link>
     </h4>
-    <hr>
+    <hr />
     <p>
-      <router-link to="/business/course" class="btn btn-white btn-default btn-round">
+      <router-link
+        to="/business/course"
+        class="btn btn-white btn-default btn-round"
+      >
         <i class="ace-icon fa fa-arrow-left"></i>
         返回课程
       </router-link>
@@ -22,43 +29,63 @@
       </button>
     </p>
 
-    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+    <pagination
+      ref="pagination"
+      v-bind:list="list"
+      v-bind:itemCount="8"
+    ></pagination>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-    <thead>
-    <tr>
-      <th>ID</th>
-      <th>名称</th>
-      <th>操作</th>
-    </tr>
-    </thead>
+    <table id="simple-table" class="table table-bordered table-hover">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>名称</th>
+          <th>操作</th>
+        </tr>
+      </thead>
 
-    <tbody>
-    <tr v-for="chapter in chapters">
-      <td>{{chapter.id}}</td>
-      <td>{{chapter.name}}</td>
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button v-on:click="toSection(chapter)" class="btn btn-white btn-xs btn-info btn-round">
-            小节
-          </button>&nbsp;
-          <button v-on:click="edit(chapter)" class="btn btn-white btn-xs btn-info btn-round">
-            编辑
-          </button>&nbsp;
-          <button v-on:click="del(chapter.id)" class="btn btn-white btn-xs btn-warning btn-round">
-            删除
-          </button>
-        </div>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+      <tbody>
+        <tr v-for="chapter in chapters">
+          <td>{{ chapter.id }}</td>
+          <td>{{ chapter.name }}</td>
+          <td>
+            <div class="hidden-sm hidden-xs btn-group">
+              <button
+                v-on:click="toSection(chapter)"
+                class="btn btn-white btn-xs btn-info btn-round"
+              >
+                小节</button
+              >&nbsp;
+              <button
+                v-on:click="edit(chapter)"
+                class="btn btn-white btn-xs btn-info btn-round"
+              >
+                编辑</button
+              >&nbsp;
+              <button
+                v-on:click="del(chapter.id)"
+                class="btn btn-white btn-xs btn-warning btn-round"
+              >
+                删除
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
             <h4 class="modal-title">表单</h4>
           </div>
           <div class="modal-body">
@@ -66,105 +93,127 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">名称</label>
                 <div class="col-sm-10">
-                  <input v-model="chapter.name" class="form-control" placeholder="名称">
+                  <input
+                    v-model="chapter.name"
+                    class="form-control"
+                    placeholder="名称"
+                  />
                 </div>
               </div>
               <div class="form-group">
                 <label class="col-sm-2 control-label">课程</label>
                 <div class="col-sm-10">
-                  <p class="form-control-static">{{course.name}}</p>
+                  <p class="form-control-static">{{ course.name }}</p>
                 </div>
               </div>
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">
+              取消
+            </button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">
+              保存
+            </button>
           </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
   </div>
 </template>
 
 <script>
-  import Pagination from "../../components/pagination";
-  export default {
-    components: {Pagination},
-    name: "chapter",
-    data: function() {
-      return {
-        chapter: {},
-        chapters: [],
-        course: {},
-      }
-    },
-    mounted: function() {
+import Pagination from "../../components/pagination";
+export default {
+  components: { Pagination },
+  name: "chapter",
+  data: function () {
+    return {
+      chapter: {},
+      chapters: [],
+      course: {},
+    };
+  },
+  mounted: function () {
+    let _this = this;
+    _this.$refs.pagination.size = 5;
+    let course = SessionStorage.get(SESSION_KEY_COURSE) || {};
+    // if (Tool.isEmpty(course)) {
+    //   _this.$router.push("/welcome");
+    // }
+    _this.course = course;
+    _this.list(1);
+    // sidebar激活样式方法一
+    this.$parent.activeSidebar("business-course-sidebar");
+  },
+  methods: {
+    /**
+     * 点击【新增】
+     */
+    add() {
       let _this = this;
-      _this.$refs.pagination.size = 5;
-      let course = SessionStorage.get(SESSION_KEY_COURSE) || {};
-      // if (Tool.isEmpty(course)) {
-      //   _this.$router.push("/welcome");
-      // }
-      _this.course = course;
-      _this.list(1);
-      // sidebar激活样式方法一
-      this.$parent.activeSidebar("business-course-sidebar");
-
+      _this.chapter = {};
+      $("#form-modal").modal("show");
     },
-    methods: {
-      /**
-       * 点击【新增】
-       */
-      add() {
-        let _this = this;
-        _this.chapter = {};
-        $("#form-modal").modal("show");
-      },
 
-      /**
-       * 点击【编辑】
-       */
-      edit(chapter) {
-        let _this = this;
-        _this.chapter = $.extend({}, chapter);
-        $("#form-modal").modal("show");
-      },
+    /**
+     * 点击【编辑】
+     */
+    edit(chapter) {
+      let _this = this;
+      _this.chapter = $.extend({}, chapter);
+      $("#form-modal").modal("show");
+    },
 
-      /**
-       * 列表查询
-       */
-      list(page) {
-        let _this = this;
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/list', {
+    /**
+     * 列表查询
+     */
+    list(page) {
+      let _this = this;
+      Loading.show();
+      _this.$ajax
+        // .post(process.env.VUE_APP_SERVER + "/business/admin/chapter/list", {
+        .post("http://127.0.0.1:9000" + "/business/admin/chapter/list", {
           page: page,
           size: _this.$refs.pagination.size,
-          courseId: _this.course.id
-        }).then((response)=>{
+          courseId: _this.course.id,
+        })
+        .then((response) => {
           Loading.hide();
           let resp = response.data;
-          _this.chapters = resp.content.list;
-          _this.$refs.pagination.render(page, resp.content.total);
+          console.log("htmlchapters", response);
 
-        })
-      },
+          // _this.chapters = resp.content.list;
+          _this.chapters = resp;
+          _this.$refs.pagination.render(page, 2);
+        });
+    },
 
-      /**
-       * 点击【保存】
-       */
-      save(page) {
-        let _this = this;
+    /**
+     * 点击【保存】
+     */
+    save(page) {
+      let _this = this;
 
-        // 保存校验
-        if (!Validator.require(_this.chapter.name, "名称")
-          || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
-          return;
-        }
-        _this.chapter.courseId = _this.course.id;
+      // 保存校验
+      if (
+        !Validator.require(_this.chapter.name, "名称") ||
+        !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)
+      ) {
+        return;
+      }
+      _this.chapter.courseId = _this.course.id;
 
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/save', _this.chapter).then((response)=>{
+      Loading.show();
+      _this.$ajax
+        .post(
+          process.env.VUE_APP_SERVER + "/business/admin/chapter/save",
+          _this.chapter
+        )
+        .then((response) => {
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
@@ -172,37 +221,41 @@
             _this.list(1);
             Toast.success("保存成功！");
           } else {
-            Toast.warning(resp.message)
+            Toast.warning(resp.message);
           }
-        })
-      },
+        });
+    },
 
-      /**
-       * 点击【删除】
-       */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除大章后不可恢复，确认删除？", function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/chapter/delete/' + id).then((response)=>{
+    /**
+     * 点击【删除】
+     */
+    del(id) {
+      let _this = this;
+      Confirm.show("删除大章后不可恢复，确认删除？", function () {
+        Loading.show();
+        _this.$ajax
+          .delete(
+            process.env.VUE_APP_SERVER + "/business/admin/chapter/delete/" + id
+          )
+          .then((response) => {
             Loading.hide();
             let resp = response.data;
             if (resp.success) {
               _this.list(1);
               Toast.success("删除成功！");
             }
-          })
-        });
-      },
+          });
+      });
+    },
 
-      /**
-       * 点击【小节】
-       */
-      toSection(chapter) {
-        let _this = this;
-        SessionStorage.set(SESSION_KEY_CHAPTER, chapter);
-        _this.$router.push("/business/section");
-      }
-    }
-  }
+    /**
+     * 点击【小节】
+     */
+    toSection(chapter) {
+      let _this = this;
+      SessionStorage.set(SESSION_KEY_CHAPTER, chapter);
+      _this.$router.push("/business/section");
+    },
+  },
+};
 </script>
