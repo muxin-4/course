6a9147d0ae5b1df1e899cde36cd1c3f3cb40e80f
muxin <wgy952046097@gmail.com>
2023-06-19 12:26:53 +0800

    feat: 1.小节管理保存和查询功能关联courseId, chapterId

--- business/src/main/java/com/course/business/controller/admin/SectionController.java
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -1,10 +1,8 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Section;
-import com.course.server.dto.SectionDto;
-import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
-import com.course.server.exception.ValidatorException;
+import com.course.server.dto.SectionDto;
+import com.course.server.dto.SectionPageDto;
 import com.course.server.service.SectionService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
@@ -12,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/section")
@@ -26,15 +23,17 @@ public class SectionController {
     /**
      * 列表查询
      *
-     * @param pageDto
+     * @param sectionPageDto
      * @return
      */
     @PostMapping("/list")
-    public ResponseDto list(@RequestBody PageDto pageDto) {
-        LOG.info("pageDto:{}", pageDto);
+    public ResponseDto list(@RequestBody SectionPageDto sectionPageDto) {
+        LOG.info("pageDto:{}", sectionPageDto);
         ResponseDto responseDto = new ResponseDto();
-        sectionService.list(pageDto);
-        responseDto.setContent(pageDto);
+        ValidatorUtil.require(sectionPageDto.getCourseId(), "课程ID");
+        ValidatorUtil.require(sectionPageDto.getChapterId(), "大章ID");
+        sectionService.list(sectionPageDto);
+        responseDto.setContent(sectionPageDto);
         return responseDto;
     }
 
